348b3375a2c35c559bf0c4a1a9f701de1f632a8f
Joel Graff <monograff76@gmail.com>
2025-09-08 21:25:26 -0500

    initial commit

---
+++ assets/player.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="player" tilewidth="16" tileheight="16" tilecount="336" columns="24">
+ <image source="assets/Modern_Interiors_Free_v2.2/Modern tiles_Free/Characters_free/Alex_16x16.png" width="384" height="224"/>
+ <tile id="120">
+  <animation>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="121" duration="100"/>
+   <frame tileid="122" duration="100"/>
+   <frame tileid="123" duration="100"/>
+   <frame tileid="124" duration="100"/>
+   <frame tileid="125" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
