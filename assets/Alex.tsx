0dcd89f59163f346582ca7f867c942429ee0314f
Joel Graff <monograff76@gmail.com>
2025-09-09 06:37:42 -0400

    file restructure

--- assets/Alex.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Alex" tilewidth="32" tileheight="48" tilecount="16" columns="4">
- <image source="assets/Modern_Interiors_RPG_Maker_Version/RPG_MAKER_XP/Characters/Alex.png" width="128" height="192"/>
-</tileset>
--- assets/player.tsx
+++ assets/player.tsx
@@ -1,14 +1,36 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="player" tilewidth="16" tileheight="16" tilecount="336" columns="24">
- <image source="assets/Modern_Interiors_Free_v2.2/Modern tiles_Free/Characters_free/Alex_16x16.png" width="384" height="224"/>
- <tile id="120">
+<tileset version="1.10" tiledversion="1.11.2" name="Alex" tilewidth="32" tileheight="48" tilecount="16" columns="4">
+ <image source="player.png" width="128" height="192"/>
+ <tile id="0">
   <animation>
-   <frame tileid="120" duration="100"/>
-   <frame tileid="121" duration="100"/>
-   <frame tileid="122" duration="100"/>
-   <frame tileid="123" duration="100"/>
-   <frame tileid="124" duration="100"/>
-   <frame tileid="125" duration="100"/>
+   <frame tileid="0" duration="430"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
   </animation>
  </tile>
 </tileset>
